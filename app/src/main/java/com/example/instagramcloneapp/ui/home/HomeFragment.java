0987fc9c375417905338ac984622c3a07076c89c
Tuba KOCABIYIK <tubakocabiyik@gmail.com>
2020-11-23 21:31:48 +0300

    update

--- app/src/main/java/com/example/instagramcloneapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/instagramcloneapp/ui/home/HomeFragment.java
@@ -11,13 +11,14 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.instagramcloneapp.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment  {
 
     private HomeViewModel homeViewModel;
 
